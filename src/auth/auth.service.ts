6f6e81579dfce12cbf7a0c28a6b0b20450b80059
kv45 <kshitijverma852@gmail.com>
2025-06-09 05:21:40 +0100

    implement local strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { UsersService } from "../users/users.service";
+import { compare } from "bcryptjs";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(private readonly usersService: UsersService) {}
+
+	async verifyUser(email: string, password: string) {
+		try {
+			const user = await this.usersService.getUser({
+				email
+			});
+			const authenticated = await compare(password, user.password);
+			if (!authenticated) {
+				throw new UnauthorizedException();
+			}
+		} catch (err) {
+			throw new UnauthorizedException("Credentials are not valid.");
+		}
+	}
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -14,6 +14,7 @@ import { User, UserSchema } from "./users.schema";
 		])
 	],
 	controllers: [UsersController],
-	providers: [UsersService]
+	providers: [UsersService],
+	exports: [UsersService]
 })
 export class UsersModule {}
