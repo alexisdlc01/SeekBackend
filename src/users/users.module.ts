b00d8ca2ee20f7358f7d03ce2f944be65c9aa44e
kv45 <kshitijverma852@gmail.com>
2025-06-09 04:41:29 +0100

    setup users schema for mongodb

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "./users.schema";
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: User.name,
+				schema: UserSchema
+			}
+		])
+	],
+	controllers: [UsersController],
+	providers: [UsersService]
 })
 export class UsersModule {}
