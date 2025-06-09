1e8914eebde55500c20fa6e60d918aecd6415023
kv45 <kshitijverma852@gmail.com>
2025-06-09 05:01:16 +0100

    make create user endpoint and service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "./users.schema";
+import { CreateUserDto } from "./dtos/create-user.dto";
+import { hash } from "bcryptjs";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(
+		@InjectModel(User.name) private readonly userModel: Model<User>
+	) {}
+
+	async create(data: CreateUserDto) {
+		await new this.userModel({
+			...data,
+			password: await hash(data.password, 10)
+		}).save();
+	}
+}
