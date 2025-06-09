c64990cf9c98f03f60314422fe6f3ececb39b4ce
kv45 <kshitijverma852@gmail.com>
2025-06-09 04:30:11 +0100

    connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true })],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		MongooseModule.forRootAsync({
+			useFactory: (config: ConfigService) => ({
+				uri: config.getOrThrow("MONGODB_URI")
+			}),
+			inject: [ConfigService]
+		})
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
