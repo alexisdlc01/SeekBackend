fff4058fd7f82670089b4c78921e34296ab58f45
kv45 <kshitijverma852@gmail.com>
2025-07-31 10:16:40 +0100

    upload to bucket works

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+	Controller,
+	FileTypeValidator,
+	MaxFileSizeValidator,
+	ParseFilePipe,
+	Post,
+	UploadedFile,
+	UseInterceptors
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { UploadService } from "./upload.service";
 
-@Controller('upload')
-export class UploadController {}
+@Controller("upload")
+export class UploadController {
+	constructor(private readonly uploadService: UploadService) {}
+
+	@Post("")
+	@UseInterceptors(FileInterceptor("file"))
+	async uploadFile(
+		@UploadedFile(
+			new ParseFilePipe({
+				validators: [
+					// TODO: Add some validation
+					// new MaxFileSizeValidator({ maxSize: 1000 }),
+					// new FileTypeValidator({ fileType: "image/jpeg" })
+				]
+			})
+		)
+		file: Express.Multer.File
+	) {
+		await this.uploadService.upload(file.originalname, file.buffer);
+	}
+}
--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
-import { UploadController } from './upload.controller';
-import { UploadService } from './upload.service';
+import { Module } from "@nestjs/common";
+import { UploadController } from "./upload.controller";
+import { UploadService } from "./upload.service";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
-  controllers: [UploadController],
-  providers: [UploadService]
+	imports: [
+		ThrottlerModule.forRoot({
+			throttlers: [
+				{
+					ttl: 60,
+					limit: 3
+				}
+			]
+		})
+	],
+	controllers: [UploadController],
+	providers: [
+		UploadService,
+		{
+			provide: APP_GUARD,
+			useClass: ThrottlerGuard
+		}
+	]
 })
 export class UploadModule {}
