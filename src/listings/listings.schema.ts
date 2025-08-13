dca29a9245cb81801b3f5702c698c7798cdd9da9
kv45 <kshitijverma852@gmail.com>
2025-08-13 06:54:39 +0100

    add verification status on listings

--- src/listings/listings.schema.ts
+++ src/listings/listings.schema.ts
@@ -69,6 +69,9 @@ export class Listing {
 
 	@Prop()
 	floorPlanImage: string;
+
+	@Prop({default: false})
+	isVerified: boolean;
 }
 
 export const ListingSchema = SchemaFactory.createForClass(Listing);
