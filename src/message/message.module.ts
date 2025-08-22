dd1a3235ba889f850a2cffe58f432162e8db205d
kv45 <kshitijverma852@gmail.com>
2025-08-22 05:09:43 +0100

    create message schema

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,4 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { SharedModule } from "../shared/shared.module";
+import { Message, MessageSchema } from "./message.schema";
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: Message.name,
+				schema: MessageSchema
+			}
+		]),
+		SharedModule
+	]
+})
 export class MessageModule {}
---
+++ src/message/message.schema.ts
@@ -0,0 +1,58 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { SchemaTypes, Types } from "mongoose";
+
+export enum MessageType {
+	Text = "Text",
+	Image = "Image"
+}
+
+@Schema()
+export class Message {
+	@Prop({ type: SchemaTypes.ObjectId, auto: true })
+	_id: Types.ObjectId;
+
+	@Prop({ type: SchemaTypes.ObjectId, ref: "User" })
+	sender?: Types.ObjectId;
+
+	@Prop({ type: SchemaTypes.ObjectId, ref: "User" })
+	botMessageTo?: Types.ObjectId;
+
+	@Prop({ enum: MessageType, default: MessageType.Text })
+	messageType: MessageType;
+
+	@Prop({ required: true, trim: true })
+	message: string;
+
+	@Prop({ default: Date.now, index: true })
+	createdAt: Date;
+
+	@Prop({ type: SchemaTypes.ObjectId, ref: "Conversation", required: true })
+	conversation: Types.ObjectId;
+
+	@Prop({ type: [SchemaTypes.ObjectId], ref: "User", default: [] })
+	seenUsers: Types.ObjectId[];
+
+	@Prop({ type: [SchemaTypes.ObjectId], ref: "User", default: [] })
+	deliveredTo: Types.ObjectId[];
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
+
+
+MessageSchema.index({ conversation: 1, createdAt: -1 });
+MessageSchema.index({ sender: 1, createdAt: -1 });
+
+
+MessageSchema.virtual("conversationDoc", {
+	ref: "Conversation",
+	localField: "conversation",
+	foreignField: "_id",
+	justOne: true
+});
+
+MessageSchema.virtual("senderDoc", {
+	ref: "User",
+	localField: "sender",
+	foreignField: "_id",
+	justOne: true
+});
