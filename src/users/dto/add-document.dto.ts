8f876004cbb12bb28326c01d88d1d2d351a91eff
kv45 <kshitijverma852@gmail.com>
2026-01-28 02:18:50 +0000

    add swagger comments to AddDocumentDto

--- src/users/dto/add-document.dto.ts
+++ src/users/dto/add-document.dto.ts
@@ -1,10 +1,13 @@
 import { IsEnum, IsString, IsUrl } from "class-validator";
 import { DocumentType } from "../types/document-type";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class AddDocumentDto {
+	@ApiProperty({enum: DocumentType})
 	@IsEnum(DocumentType)
 	documentType: DocumentType;
 
+	@ApiProperty()
 	@IsUrl()
 	url: string;
 }
